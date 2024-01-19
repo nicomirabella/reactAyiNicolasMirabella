914509d0bdef383e5aa84b4461fe3456014ba311
nicomirabella <nicolas.llanir@ayi.group>
2024-01-19 13:35:41 -0300

    Agregar archivos a entregable React

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react'
+import Formulario from "./components/Formulario"
+import Header from "./components/Header"
+import ListadoPaciente from "./components/ListadoPaciente"
+
+
+function App() {
+
+  const [pacientes, setPacientes] = useState([]);
+  const [paciente, setPaciente] = useState ({});
+
+  useEffect(() => {
+    const obtenerLS = () => {
+      const pacientesLS = JASON.parse(loalStorage.getItem('pacientes')) ?? [];
+      setPacientes(pacientesLS)
+    }
+
+    obtenerLS();
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JASON.stingify( pacientes ));
+  }, [pacientes])
+
+  const eliminarPaciente = id => {
+    const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id);
+    setPacientes(pacientesActualizados)
+  }
+
+  
+
+  return (
+    <div className="container mx-auto mt-20">
+     <Header 
+     />
+
+     <div className="mt-12 md:flex">
+      <Formulario 
+        pacientes={pacientes} 
+        setPacientes={setPacientes} 
+        paciente={paciente}  
+        setPaciente={setPaciente}     
+      />
+      <ListadoPaciente 
+        pacientes={pacientes}
+        setPaciente={setPaciente}
+        eliminarPaciente={eliminarPaciente}
+      
+      />
+     </div> 
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ListadoPaciente.jsx
@@ -0,0 +1,46 @@
+import Paciente from "./paciente"
+
+
+const ListadoPaciente = ({pacientes, setPaciente, eliminarPaciente}) => {
+
+ 
+  return (
+    
+    <div className='md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll'>
+
+      {pacientes && pacientes.length ? (
+
+     
+      <>
+          <h2 className='font-black text-3xl text-center'>Listado Pacientes
+          </h2>
+          <p className='text-xl mt-5 mb-10 text-center'>
+            Administra tus {''}
+            <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
+          </p>
+
+          { pacientes.map( paciente => (
+              <Paciente 
+                key={paciente.id}
+                paciente={paciente}
+                setPaciente={setPaciente}
+                eliminarPaciente={eliminarPaciente}
+              />
+            ))}
+      </>
+       ) : (
+        <>
+             <h2 className='font-black text-3xl text-center'>No Hay Pacientes
+            </h2>
+             <p className='text-xl mt-5 mb-10 text-center'>
+               Comienza Agregando Pacientes {''}
+             <span className='text-indigo-600 font-bold'>y van a aparecer en este lugar</span>
+            </p>
+        
+        </>
+       )}
+    </div>
+  )
+}
+
+export default ListadoPaciente
